8a5beaff832c04adf33cbed27a043a987a16b5b1
Mirko Seifert <mirko.seifert@devboost.de>
2012-08-19 19:16:39 +0200

    added copyright headers

--- JenkinsPlugin/org.buildboost.hudson.plugins.boostscm/src/main/java/org/buildboost/hudson/plugins/boostscm/IFunction.java
+++ JenkinsPlugin/org.buildboost.hudson.plugins.boostscm/src/main/java/org/buildboost/hudson/plugins/boostscm/IFunction.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2012
+ * DevBoost GmbH, Berlin, Amtsgericht Charlottenburg, HRB 140026
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   DevBoost GmbH - Berlin, Germany
+ *      - initial API and implementation
+ ******************************************************************************/
 package org.buildboost.hudson.plugins.boostscm;
 
 public interface IFunction<ReturnType, ArgumentType1> {
