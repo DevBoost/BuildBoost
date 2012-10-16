70f3fb9d0e35d4fb5eae3dfb225ff60fa8036e7a
Jendrik Johannes <jendrik.johannes@devboost.de>
2012-10-16 15:38:45 +0200

    changed clone stage to process all locations together which allows merging of (sub-)locations from multiple repository files

--- Core/de.devboost.buildboost/src/de/devboost/buildboost/steps/clone/CloneRepositoriesBuildStepProvider.java
+++ Core/de.devboost.buildboost/src/de/devboost/buildboost/steps/clone/CloneRepositoriesBuildStepProvider.java
@@ -17,21 +17,29 @@ package de.devboost.buildboost.steps.clone;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
-import de.devboost.buildboost.ant.AbstractAntTargetGeneratorProvider;
+import de.devboost.buildboost.BuildException;
+import de.devboost.buildboost.UnresolvedDependencyChecker;
 import de.devboost.buildboost.ant.IAntTargetGenerator;
+import de.devboost.buildboost.ant.IAntTargetGeneratorProvider;
 import de.devboost.buildboost.artifacts.RepositoriesFile;
 import de.devboost.buildboost.artifacts.RepositoriesFile.Location;
+import de.devboost.buildboost.model.AbstractBuildParticipant;
 import de.devboost.buildboost.model.IArtifact;
+import de.devboost.buildboost.model.IArtifactDiscoverer;
+import de.devboost.buildboost.model.IArtifactFilter;
 import de.devboost.buildboost.model.IBuildContext;
+import de.devboost.buildboost.model.IBuildParticipant;
 
 /**
  * The {@link CloneRepositoriesBuildStepProvider} add a {@link CloneRepositoriesBuildStep} 
  * for each {@link RepositoriesFile}.
  */
-public class CloneRepositoriesBuildStepProvider extends AbstractAntTargetGeneratorProvider {
+public class CloneRepositoriesBuildStepProvider extends AbstractBuildParticipant
+		implements IAntTargetGeneratorProvider {
 
 	private File reposFolder;
 	
@@ -39,19 +47,69 @@ public class CloneRepositoriesBuildStepProvider extends AbstractAntTargetGenerat
 		super();
 		this.reposFolder = reposFolder;
 	}
-
-	public List<IAntTargetGenerator> getAntTargetGenerators(IBuildContext context, IArtifact artifact) {
-		if (artifact instanceof RepositoriesFile) {
-			RepositoriesFile repositoriesFile = (RepositoriesFile) artifact;
-			List<IAntTargetGenerator> steps = new ArrayList<IAntTargetGenerator>();
-			List<Location> locations = repositoriesFile.getLocations();
-			for (Location location : locations) {
-				CloneRepositoriesBuildStep step = new CloneRepositoriesBuildStep(
-						reposFolder, location);
-				steps.add(step);
-			}
-			return steps;
-		}
-		return Collections.emptyList();
+	
+	public void execute(IBuildContext context) throws BuildException {
+		List<IArtifact> antTargets = new ArrayList<IArtifact>();
+		Collection<IArtifact> artifacts = context.getDiscoveredArtifacts();
+		antTargets.add(getAntTargetGenerators(context, artifacts));
+		context.addDiscoveredArtifacts(antTargets);
 	}
+
+	public IAntTargetGenerator getAntTargetGenerators(IBuildContext context, Collection<IArtifact> artifacts) {
+		Collection<Location> locations = new ArrayList<RepositoriesFile.Location>();
+		for (IArtifact artifact : artifacts) {
+			if (artifact instanceof RepositoriesFile) {
+				RepositoriesFile repositoriesFile = (RepositoriesFile) artifact;
+				for (Location location : repositoriesFile.getLocations()) {
+					boolean locationDuplicate = false;
+					for (Location locationInList : locations) {
+						if (locationInList.getUrl().equals(location.getUrl())) {
+							locationDuplicate = true;
+							locationInList.getSubDirectories().addAll(location.getSubDirectories());
+							break;
+						}
+					}
+					if (!locationDuplicate) {
+						locations.add(location);
+					}
+				}
+			}
+		}
+		
+		removeOverlapsInSVNLocations(locations);
+		
+		CloneRepositoriesBuildStep step = new CloneRepositoriesBuildStep(reposFolder, locations);
+		return step;
+	}
+	
+	private void removeOverlapsInSVNLocations(Collection<Location> locations) {
+		for (Location location1 : new ArrayList<Location>(locations)) {
+			if (!"svn".equals(location1.getType())) {
+				continue;
+			}
+			for (Iterator<Location> i = locations.iterator(); i.hasNext();) {
+				Location location2 = i.next();
+				if (!"svn".equals(location2.getType())) {
+					continue;
+				}
+				if (!location1.equals(location2) && location2.getUrl().startsWith(location1.getUrl())) {
+					i.remove();
+				}
+			}
+		}
+	}
+
+	public boolean dependsOn(IBuildParticipant otherParticipant) {
+		if (otherParticipant instanceof IArtifactDiscoverer) {
+			return true;
+		}
+		if (otherParticipant instanceof IArtifactFilter) {
+			return true;
+		}
+		if (otherParticipant instanceof UnresolvedDependencyChecker) {
+			return true;
+		}
+		return false;
+	}
+
 }
