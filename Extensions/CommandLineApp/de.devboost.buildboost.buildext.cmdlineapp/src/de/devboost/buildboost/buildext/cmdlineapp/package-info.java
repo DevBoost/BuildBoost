caa78c316bda28188ae9ebb930e8789f833f980c
Mirko Seifert <mirko.seifert@devboost.de>
2013-03-01 17:20:50 +0100

    removed old plug-in folder

--- Extensions/CommandLineApp/de.devboost.buildboost.buildext.cmdlineapp/src/de/devboost/buildboost/buildext/cmdlineapp/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains class that extend BuildBoost to automatically bundle
- * command line applications as executable JAR files.
- */
-package de.devboost.buildboost.buildext.cmdlineapp;
