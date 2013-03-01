a8af68d420a100aa68cf22a42ed7f7387261f7a0
Mirko Seifert <mirko.seifert@devboost.de>
2013-03-01 16:50:17 +0100

    added BuildBoost extension for command line apps (copied from AutoBuild sources)

---
+++ Extensions/CommandLineApp/de.devboost.buildboost.buildext.cmdlineapp/src/de/devboost/buildboost/buildext/cmdlineapp/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains class that extend BuildBoost to automatically bundle
+ * command line applications as executable JAR files.
+ */
+package de.devboost.buildboost.buildext.cmdlineapp;
