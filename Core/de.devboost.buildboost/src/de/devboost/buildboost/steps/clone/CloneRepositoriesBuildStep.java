4fe4f55758dd272337d58d56929c7d1f764f7c83
Mirko Seifert <mirko.seifert@devboost.de>
2013-03-21 21:59:51 +0100

    refactored CloneRepositoriesBuildStep

--- Core/de.devboost.buildboost/src/de/devboost/buildboost/steps/clone/CloneRepositoriesBuildStep.java
+++ Core/de.devboost.buildboost/src/de/devboost/buildboost/steps/clone/CloneRepositoriesBuildStep.java
@@ -25,7 +25,10 @@ import de.devboost.buildboost.artifacts.RepositoriesFile.Location;
 import de.devboost.buildboost.util.AntScriptUtil;
 import de.devboost.buildboost.util.XMLContent;
 
-
+/**
+ * The {@link CloneRepositoriesBuildStep} generates Ant task to clone/check 
+ * out/update/pull/download repositories.
+ */
 public class CloneRepositoriesBuildStep extends AbstractAntTargetGenerator {
 
 	private File reposFolder;
@@ -42,125 +45,164 @@ public class CloneRepositoriesBuildStep extends AbstractAntTargetGenerator {
 		result.add(writeRepositoryList());
 		
 		for (Location location : locations) {
-			String locationURL = location.getUrl();
-			String localRepositoryFolderName = url2FolderName(locationURL);
-			
-			String rootName = url2FolderName(locationURL.substring(locationURL.lastIndexOf("/") + 1));
-
-			File localRepo = new File(new File(reposFolder, localRepositoryFolderName), rootName);
-			
-			XMLContent content = new XMLContent();
-			
-			content.append("<condition property=\"git-executable\" value=\"git.cmd\">");
-			content.append("<os family=\"windows\"/>");
-			content.append("</condition>");
-			content.append("<condition property=\"git-executable\" value=\"git\">");
-			content.append("<not>");
-			content.append("<os family=\"windows\"/>");
-			content.append("</not>");
-			content.append("</condition>");
-				
-			boolean isGit = location.getType().equals("git");
-			boolean isSVN = location.getType().equals("svn");
-			
-			String localRepositoryPath = getLocalRepositoryPath(location);
-			
-			if (isGit) {
-				if (localRepo.exists()) {
-					content.append("<exec executable=\"${git-executable}\" dir=\"" + localRepositoryPath + "\" failonerror=\"false\">");
-					content.append("<arg value=\"pull\"/>");
-					content.append("</exec>");
-				} else {
-					content.append("<mkdir dir=\"" + localRepositoryPath + "\">");
-					content.append("</mkdir>");
-					content.append("<exec executable=\"${git-executable}\" dir=\"" + reposFolder.getAbsolutePath() + "\" failonerror=\"false\">");
-					content.append("<arg value=\"clone\"/>");
-					content.append("<arg value=\"" + locationURL + "\"/>");
-					content.append("<arg value=\"" + localRepositoryPath + "\"/>");
-					content.append("</exec>");
-				}
-				if (!location.getSubDirectories().isEmpty()) {
-					//enable sparse checkout
-					content.append("<exec executable=\"${git-executable}\" dir=\"" + localRepositoryPath + "\" failonerror=\"false\">");
-					content.append("<arg value=\"config\"/>");
-					content.append("<arg value=\"core.sparsecheckout\"/>");
-					content.append("<arg value=\"true\"/>");
-					content.append("</exec>");
-					String dirList = ".gitignore${line.separator}";
-					for (String subDir : location.getSubDirectories()) {
-						dirList += subDir;
-						dirList += "${line.separator}";
-					}
-					content.append("<echo message=\"" + dirList + "\" file=\"" + localRepositoryPath + "/.git/info/sparse-checkout\"/>");
-					content.append("<exec executable=\"${git-executable}\" dir=\"" + localRepositoryPath + "\" failonerror=\"false\">");
-					content.append("<arg value=\"read-tree\"/>");
-					content.append("<arg value=\"-mu\"/>");
-					content.append("<arg value=\"HEAD\"/>");
-					content.append("</exec>");
-				}
-			} else if (isSVN) {
-				if (localRepo.exists()) {
-					// execute update
-					content.append("<exec executable=\"svn\" dir=\"" + localRepositoryPath + "\" failonerror=\"false\">");
-					content.append("<arg value=\"update\"/>");
-					content.append("</exec>");
-				} else {
-					// execute checkout
-					content.append("<exec executable=\"svn\" dir=\"" + reposFolder + "\" failonerror=\"false\">");
-					content.append("<arg value=\"co\"/>");
-					content.append("<arg value=\"" + locationURL + "\"/>");
-					content.append("<arg value=\"" + localRepositoryPath + "\"/>");
-					content.append("</exec>");
-				}
-			} else /* isGet */ {
-				if (!localRepo.exists()) {
-					content.append("<mkdir dir=\""+ localRepositoryPath + "\"/>");
-					if (!location.getSubDirectories().isEmpty()) {
-						if (localRepo.getName().endsWith(".zip")) {
-							String zipFilePath = new File(localRepo, rootName).getAbsolutePath()  ;
-							content.append("<get src=\""+ locationURL + "\" dest=\""+ localRepositoryPath + "\"/>");
-							content.append("<unzip src=\"" + zipFilePath + "\" dest=\"" +  localRepositoryPath + "\">");
-							content.append("<patternset>");
-							for (String zipEntry : location.getSubDirectories()) {
-								content.append("<include name=\"" + zipEntry + "\"/>");
-							}
-							content.append("</patternset>");
-							content.append("</unzip>");
-							content.append("<delete file=\"" + zipFilePath + "\"/>");
-						} else /* folder */ {
-							for (String subPath : location.getSubDirectories()) {
-								content.append("<get src=\""+ locationURL + "/" + subPath + "\" dest=\""+ localRepositoryPath + "/" + subPath + "\"/>");
-							}	
-						}
-					} else {
-						AntScriptUtil.addDownloadFileScript(content, locationURL, localRepositoryPath);
-					}
-				}
-			}
-			result.add(new AntTarget("update-" + localRepositoryFolderName, content));
+			generateCloneLocationTasks(result, location);
 		}
 		return result;
 	}
 
+	private void generateCloneLocationTasks(Collection<AntTarget> result,
+			Location location) {
+		String locationURL = location.getUrl();
+		String localRepositoryFolderName = url2FolderName(locationURL);
+		
+		String rootName = url2FolderName(locationURL.substring(locationURL.lastIndexOf("/") + 1));
+
+		File localRepo = new File(new File(reposFolder, localRepositoryFolderName), rootName);
+		
+		XMLContent content = new XMLContent();
+		
+		content.append("<condition property=\"git-executable\" value=\"git.cmd\">");
+		content.append("<os family=\"windows\"/>");
+		content.append("</condition>");
+		content.append("<condition property=\"git-executable\" value=\"git\">");
+		content.append("<not>");
+		content.append("<os family=\"windows\"/>");
+		content.append("</not>");
+		content.append("</condition>");
+			
+		String locationType = location.getType();
+		
+		boolean isGit = locationType.equals("git");
+		boolean isSVN = locationType.equals("svn");
+		
+		String localRepositoryPath = getLocalRepositoryPath(location);
+		
+		if (isGit) {
+			addCloneGitRepositoryTasks(location, locationURL, localRepo,
+					content, localRepositoryPath);
+		} else if (isSVN) {
+			addCloneSVNTasks(locationURL, localRepo, content,
+					localRepositoryPath);
+		} else /* isGet */ {
+			addCloneOtherTasks(location, locationURL, rootName, localRepo,
+					content, localRepositoryPath);
+		}
+		result.add(new AntTarget("update-" + localRepositoryFolderName, content));
+	}
+
+	private void addCloneOtherTasks(Location location, String locationURL,
+			String rootName, File localRepo, XMLContent content,
+			String localRepositoryPath) {
+		
+		if (localRepo.exists()) {
+			return;
+		}
+		
+		content.append("<mkdir dir=\""+ localRepositoryPath + "\"/>");
+		if (!location.getSubDirectories().isEmpty()) {
+			if (localRepo.getName().endsWith(".zip")) {
+				String zipFilePath = new File(localRepo, rootName).getAbsolutePath()  ;
+				content.append("<get src=\""+ locationURL + "\" dest=\""+ localRepositoryPath + "\"/>");
+				content.append("<unzip src=\"" + zipFilePath + "\" dest=\"" +  localRepositoryPath + "\">");
+				content.append("<patternset>");
+				for (String zipEntry : location.getSubDirectories()) {
+					content.append("<include name=\"" + zipEntry + "\"/>");
+				}
+				content.append("</patternset>");
+				content.append("</unzip>");
+				content.append("<delete file=\"" + zipFilePath + "\"/>");
+			} else /* folder */ {
+				for (String subPath : location.getSubDirectories()) {
+					content.append("<get src=\""+ locationURL + "/" + subPath + "\" dest=\""+ localRepositoryPath + "/" + subPath + "\"/>");
+				}	
+			}
+		} else {
+			AntScriptUtil.addDownloadFileScript(content, locationURL, localRepositoryPath);
+		}
+	}
+
+	/**
+	 * Adds Ant tasks to check out or update an SVN repository.
+	 */
+	private void addCloneSVNTasks(String locationURL, File localRepo,
+			XMLContent content, String localRepositoryPath) {
+		if (localRepo.exists()) {
+			// execute update
+			content.append("<exec executable=\"svn\" dir=\"" + localRepositoryPath + "\" failonerror=\"false\">");
+			content.append("<arg value=\"update\"/>");
+			content.append("</exec>");
+		} else {
+			// execute checkout
+			content.append("<exec executable=\"svn\" dir=\"" + reposFolder + "\" failonerror=\"false\">");
+			content.append("<arg value=\"co\"/>");
+			content.append("<arg value=\"" + locationURL + "\"/>");
+			content.append("<arg value=\"" + localRepositoryPath + "\"/>");
+			content.append("</exec>");
+		}
+	}
+
+	/**
+	 * Adds Ant tasks to clone or pull a GIT repository.
+	 */
+	private void addCloneGitRepositoryTasks(Location location,
+			String locationURL, File localRepo, XMLContent content,
+			String localRepositoryPath) {
+		if (localRepo.exists()) {
+			content.append("<exec executable=\"${git-executable}\" dir=\"" + localRepositoryPath + "\" failonerror=\"false\">");
+			content.append("<arg value=\"pull\"/>");
+			content.append("</exec>");
+		} else {
+			content.append("<mkdir dir=\"" + localRepositoryPath + "\">");
+			content.append("</mkdir>");
+			content.append("<exec executable=\"${git-executable}\" dir=\"" + reposFolder.getAbsolutePath() + "\" failonerror=\"false\">");
+			content.append("<arg value=\"clone\"/>");
+			content.append("<arg value=\"" + locationURL + "\"/>");
+			content.append("<arg value=\"" + localRepositoryPath + "\"/>");
+			content.append("</exec>");
+		}
+		if (!location.getSubDirectories().isEmpty()) {
+			//enable sparse checkout
+			content.append("<exec executable=\"${git-executable}\" dir=\"" + localRepositoryPath + "\" failonerror=\"false\">");
+			content.append("<arg value=\"config\"/>");
+			content.append("<arg value=\"core.sparsecheckout\"/>");
+			content.append("<arg value=\"true\"/>");
+			content.append("</exec>");
+			String dirList = ".gitignore${line.separator}";
+			for (String subDir : location.getSubDirectories()) {
+				dirList += subDir;
+				dirList += "${line.separator}";
+			}
+			content.append("<echo message=\"" + dirList + "\" file=\"" + localRepositoryPath + "/.git/info/sparse-checkout\"/>");
+			content.append("<exec executable=\"${git-executable}\" dir=\"" + localRepositoryPath + "\" failonerror=\"false\">");
+			content.append("<arg value=\"read-tree\"/>");
+			content.append("<arg value=\"-mu\"/>");
+			content.append("<arg value=\"HEAD\"/>");
+			content.append("</exec>");
+		}
+	}
+
 	private AntTarget writeRepositoryList() {
 		XMLContent content = new XMLContent();
-		String revisionFile = new File(reposFolder, "buildboost_repository_list.txt").getAbsolutePath();
-		content.append("<echo message=\"\" file=\"" + revisionFile + "\" append=\"false\">");
+		File repositoryListFile = new File(reposFolder, "buildboost_repository_list.txt");
+		String repositoryListPath = repositoryListFile.getAbsolutePath();
+		content.append("<echo message=\"\" file=\"" + repositoryListPath + "\" append=\"false\">");
 		content.append("</echo>");
 		for (Location location : locations) {
-			boolean isGit = location.getType().equals("git");
-			boolean isSVN = location.getType().equals("svn");
+			String locationType = location.getType();
+			
+			boolean isGit = locationType.equals("git");
+			boolean isSVN = locationType.equals("svn");
 			
 			String locationURL = location.getUrl();
 			
 			String localRepositoryPath = getLocalRepositoryPath(location); 
 			
 			if (isSVN || isGit) {
-				content.append("<echo message=\"BuildBoost-Repository-Type: " + location.getType() + "\" file=\"" + revisionFile + "\" append=\"true\">");
+				content.append("<echo message=\"BuildBoost-Repository-Type: " + locationType + "\" file=\"" + repositoryListPath + "\" append=\"true\">");
 				content.append("</echo>");
-				content.append("<echo message=\"BuildBoost-Repository-URL: " + locationURL + "\" file=\"" + revisionFile + "\" append=\"true\">");
+				content.append("<echo message=\"BuildBoost-Repository-URL: " + locationURL + "\" file=\"" + repositoryListPath + "\" append=\"true\">");
 				content.append("</echo>");
-				content.append("<echo message=\"BuildBoost-Repository-Local: " + localRepositoryPath + "\" file=\"" + revisionFile + "\" append=\"true\">");
+				content.append("<echo message=\"BuildBoost-Repository-Local: " + localRepositoryPath + "\" file=\"" + repositoryListPath + "\" append=\"true\">");
 				content.append("</echo>");
 			}
 		}
