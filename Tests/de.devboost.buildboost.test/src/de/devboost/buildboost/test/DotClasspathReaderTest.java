47213e133a892326bc1d8b0391f14c50fd713bd4
Mirko Seifert <mirko.seifert@devboost.de>
2013-07-01 16:34:30 +0200

    added JUnit test for DotClasspathReader class

---
+++ Tests/de.devboost.buildboost.test/src/de/devboost/buildboost/test/DotClasspathReaderTest.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2013
+ * Software Technology Group, Dresden University of Technology
+ * DevBoost GmbH, Berlin, Amtsgericht Charlottenburg, HRB 140026
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Software Technology Group - TU Dresden, Germany;
+ *   DevBoost GmbH - Berlin, Germany
+ *      - initial API and implementation
+ ******************************************************************************/
+package de.devboost.buildboost.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+
+import org.junit.Test;
+
+import de.devboost.buildboost.discovery.reader.DotClasspathReader;
+
+public class DotClasspathReaderTest {
+
+	@Test
+	public void testLibReading() throws IOException {
+		String input = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+				"<classpath>" +
+				"<classpathentry kind=\"con\" path=\"org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.6\"/>" +
+				"<classpathentry kind=\"con\" path=\"org.eclipse.pde.core.requiredPlugins\"/>" +
+				"<classpathentry kind=\"src\" path=\"src\"/>" +
+				"<classpathentry kind=\"lib\" path=\"lib/some-third-party-lib-1.0.0.jar\"/>" +
+				"<classpathentry kind=\"lib\" path=\"lib/some-lib-with-source-2.0.0.jar\" sourcepath=\"/Users/Me/Downloads/some-lib-with-source-2.0.0-src.zip\"/>" +
+				"<classpathentry kind=\"output\" path=\"bin\"/>" +
+				"</classpath>";
+		
+		byte[] bytes = input.getBytes();
+		InputStream inputStream = new ByteArrayInputStream(bytes);
+		
+		DotClasspathReader reader = new DotClasspathReader(inputStream);
+		Set<String> dependencies = reader.getDependencies();
+		assertEquals("Unexpected number of dependencies", 2, dependencies.size());
+		
+		assertTrue(dependencies.contains("lib/some-third-party-lib-1.0.0.jar"));
+		assertTrue(dependencies.contains("lib/some-lib-with-source-2.0.0.jar"));
+	}
+}
