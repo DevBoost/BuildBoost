b067abe1d5981e297bb422d57e98cd6f45e9338b
Mirko Seifert <mirko.seifert@devboost.de>
2013-07-20 14:07:15 +0200

    Added documentation Added toString() to ease debugging

--- Core/de.devboost.buildboost/src/de/devboost/buildboost/model/ResolvedDependency.java
+++ Core/de.devboost.buildboost/src/de/devboost/buildboost/model/ResolvedDependency.java
@@ -15,6 +15,12 @@
  ******************************************************************************/
 package de.devboost.buildboost.model;
 
+/**
+ * A {@link ResolvedDependency} replaces a {@link UnresolvedDependency} when the
+ * respective dependency is resolved. It provides access to the origin of the
+ * dependency (see {@link #getSource()} and the required artifact (see
+ * {@link #getTarget()}).
+ */
 public class ResolvedDependency {
 
 	private IArtifact source;
@@ -39,5 +45,14 @@ public class ResolvedDependency {
 
 	public UnresolvedDependency getUnresolvedDependency() {
 		return unresolvedDependency;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("ResolvedDependency [target=");
+		builder.append(target);
+		builder.append("]");
+		return builder.toString();
 	}
 }
