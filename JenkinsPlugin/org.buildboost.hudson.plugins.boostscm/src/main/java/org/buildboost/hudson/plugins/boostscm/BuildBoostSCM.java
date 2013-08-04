e31aa2f7c7dd18ac3e8167530b9213ca96ac323b
Mirko Seifert <mirko.seifert@devboost.de>
2013-08-04 14:13:15 +0200

    Fixed problem with dynamic files which are not references via HTTP, but which are located in the local file system

--- JenkinsPlugin/org.buildboost.hudson.plugins.boostscm/src/main/java/org/buildboost/hudson/plugins/boostscm/BuildBoostSCM.java
+++ JenkinsPlugin/org.buildboost.hudson.plugins.boostscm/src/main/java/org/buildboost/hudson/plugins/boostscm/BuildBoostSCM.java
@@ -39,6 +39,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -328,9 +329,12 @@ public class BuildBoostSCM extends SCM {
 	public String getContent(String urlString) {
 		try {
 			URL url = new URL(urlString);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-			InputStream inputStream = conn.getInputStream();
+			URLConnection connection = url.openConnection();
+			if (connection instanceof HttpURLConnection) {
+				HttpURLConnection httpConnection = (HttpURLConnection) connection;
+				httpConnection.setRequestMethod("GET");
+			}
+			InputStream inputStream = connection.getInputStream();
 			return getContent(inputStream);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
