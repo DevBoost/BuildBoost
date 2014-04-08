4952003c38f6856fed1171a2f5c04f295e412591
Mirko Seifert <mirko.seifert@devboost.de>
2014-04-08 10:15:24 +0200

    Added missing copyright header Added TODO item

--- Core/de.devboost.buildboost/src/de/devboost/buildboost/artifacts/InvalidMetadataException.java
+++ Core/de.devboost.buildboost/src/de/devboost/buildboost/artifacts/InvalidMetadataException.java
@@ -1,9 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2014
+ * Software Technology Group, Dresden University of Technology
+ * DevBoost GmbH, Berlin, Amtsgericht Charlottenburg, HRB 140026
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Software Technology Group - TU Dresden, Germany;
+ *   DevBoost GmbH - Berlin, Germany
+ *      - initial API and implementation
+ ******************************************************************************/
 package de.devboost.buildboost.artifacts;
 
 /**
  * An {@link InvalidMetadataException} is thrown if meta data is missing from a
  * plug-in (e.g., if it does not have a symbolic name).
  */
+// TODO Instead of throwing this exception, the discovery logic for plug-ins
+// should check the meta data of plug-ins.
 public class InvalidMetadataException extends Exception {
 
 	private static final long serialVersionUID = -9207028531518266417L;
